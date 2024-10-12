d9bf95fdd642bec10b18932e8de0eaee67a295aa
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2024-10-12 12:56:17 -0300

    Teste de conexão com o Postgres usando pool.

---
+++ api/database.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    database: process.env.DATABASE
+});
+
+(async => () {
+    const client = await pool.connect();
+})
--- api/index.js
+++ api/index.js
@@ -1,12 +1,47 @@
+require('dotenv').config({ override: true }); // Carrega as variáveis de ambiente
+
 const express = require("express");
 const app = express();
-
+const database = require('./database');
 const PORT = 3000;
 
-app.get('/', (request, response) => {
-    response.send('A API está funcionando');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    database: process.env.DATABASE
 });
 
-app.listen(PORT, () => {
-    console.log("Servidor rodando em http://localhost:3000");
-});
+(async function test() {
+    const client = await pool.connect();
+    
+    try {
+        const { rows } = await client.query('SELECT current_user');
+        const currentUser = rows[0]['current_user'];
+        console.log(currentUser);
+    }
+    catch (error) {
+        console.error(error);
+    }
+    finally {
+        client.release();
+    }
+}) 
+
+test();
+
+
+// app.get('/', (request, response) => {
+//     response.send('A API está funcionando');
+// });
+
+// app.get('/tarefas-usuario', (request, response) => {
+//     response.send('Buscando dados no banco');
+// });
+
+// app.listen(PORT, () => {
+//     console.log("Servidor rodando em http://localhost:3000");
+// });
