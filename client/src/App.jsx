7e550de3487c9a247074718fc8880929b87fe858
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2024-10-12 11:02:37 -0300

    Componente Tarefa adicionado e estilizado.

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Tarefa from './components/Tarefa';
+
+function App() {
+  return (
+    <div className="card">
+      <h1 className="titulo-app">Welcome, Vinícius</h1> <br />
+      {/* <Formulario /> */}
+      <Tarefa />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Tarefa.jsx
@@ -0,0 +1,15 @@
+import './Tarefa.css';
+
+export default function Tarefa() {
+    return (
+        <div className="tarefa">
+            <div className='dados-tarefa'>
+                <input type="checkbox" id="checkbox" /> &nbsp;
+                <input type="text" id="titulo-tarefa" placeholder="Título"/> &nbsp;
+                <input type="date" id="data" /> &nbsp;
+                <span className="material-icons">delete_forever</span> <br />
+            </div>
+            <textarea id="descricao" placeholder='Descrição'></textarea>
+        </div>
+    );
+}
