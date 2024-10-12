cbc14e3f8f0c712a35a08164b6bedcad9faea6d6
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2024-10-12 13:56:09 -0300

    Adicionei a consulta de todas as tarefas para um certo usuário (por enquanto hard coded).

--- api/database.js
+++ api/database.js
@@ -1,13 +1,28 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.PORT,
-    database: process.env.DATABASE
-});
+class Database {
+    pool = new Pool({
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        host: process.env.HOST,
+        port: process.env.PORT,
+        database: process.env.DATABASE
+    });
 
-(async => () {
-    const client = await pool.connect();
-})
+    // Retorna todas as tarefas para um usuário especificado
+    async selectTarefasUsuario(idUsuario) {
+        const client = await this.pool.connect();
+
+        try {
+            return await client.query("SELECT * FROM tarefas WHERE id_usuario = " + idUsuario + ";").rows;
+        }
+        catch (erro) {
+            console.error(erro);
+        }
+        finally {
+            client.release();
+        }
+    }
+}
+
+module.exports = { Database };
--- api/index.js
+++ api/index.js
@@ -1,47 +1,23 @@
-require('dotenv').config({ override: true }); // Carrega as variáveis de ambiente
-
+// IMPORTS
 const express = require("express");
 const app = express();
-const database = require('./database');
+const { Database } = require('./database');
+
+require('dotenv').config({ override: true }); // Carrega as variáveis de ambiente
+const database = new Database();
 const PORT = 3000;
 
-const { Pool } = require('pg');
-
-const pool = new Pool({
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.PORT,
-    database: process.env.DATABASE
+// ROTAS
+app.get('/', (request, response) => {
+    response.send('A API está funcionando');
 });
 
-(async function test() {
-    const client = await pool.connect();
-    
-    try {
-        const { rows } = await client.query('SELECT current_user');
-        const currentUser = rows[0]['current_user'];
-        console.log(currentUser);
-    }
-    catch (error) {
-        console.error(error);
-    }
-    finally {
-        client.release();
-    }
-}) 
+app.get('/tarefas-usuario', async (request, response) => {
+    console.log('Buscando dados no banco...');
+    let tarefas = await database.selectTarefasUsuario(1);
+});
 
-test();
-
-
-// app.get('/', (request, response) => {
-//     response.send('A API está funcionando');
-// });
-
-// app.get('/tarefas-usuario', (request, response) => {
-//     response.send('Buscando dados no banco');
-// });
-
-// app.listen(PORT, () => {
-//     console.log("Servidor rodando em http://localhost:3000");
-// });
+// SUBINDO O SERVIDOR
+app.listen(PORT, () => {
+    console.log("Servidor rodando em http://localhost:3000");
+});
