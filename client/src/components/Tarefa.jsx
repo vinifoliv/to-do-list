b88416ad57040f0b086317a3784b32167d937f3f
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2024-10-13 16:39:40 -0300

    Todas as tarefas com checked foi resolvido. Adição de hover sobre o botão de remover para 'vivificá-lo'

--- client/src/components/Tarefa.jsx
+++ client/src/components/Tarefa.jsx
@@ -1,15 +1,13 @@
 import './Tarefa.css';
 
 export default function Tarefa(props) {
-    console.log(props.dados);
-
     return (
         <div className="tarefa" id={ props.dados['id'] }>
             <div className='dados-tarefa'>
                 {
                     /* Renderizacao do checkbox checked ou nao */
                     props.dados['completa'] === true ? <input type="checkbox" className="checkbox" checked/> :
-                    <input type="checkbox" className="checkbox" checked/>
+                    <input type="checkbox" className="checkbox" />
                 } &nbsp;
                 <input type="text" className="titulo-tarefa"value={ props.dados['titulo'] }/> &nbsp;
                 <input type="date" className="data" value={ props.dados['vencimento'].split('T')[0] }/> &nbsp;
