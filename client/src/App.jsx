29fc7e0dd5d35dd63d948c9b9be60ad761a0420a
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2024-10-12 22:59:03 -0300

    Adicionado formulário para a inclusão de tarefas.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
-// import { useState } from 'react';
 import './App.css';
 import Formulario from './components/Formulario';
 import Tarefa from './components/Tarefa';
 
-// const DOMAIN = 'localhost:9000';
-
 function App() {
-  // const [ tarefas, setTarefas ] = useState(null);
 
   return (
     <div className="card">
       <h1 className="titulo-app">Welcome, Vinícius</h1> <br />
+
+      <h3>Insira aqui a sua tarefa</h3>
       <Formulario />
+
+      <h3>Suas tarefas</h3>
       <Tarefa />
     </div>
   );
--- client/src/components/Formulario.jsx
+++ client/src/components/Formulario.jsx
@@ -1,18 +1,12 @@
-import './Formulario.css';
-
 export default function Formulario() {
     return (
-        <form className="formulario">
-            <div className="tarefa">
-                <h3>Insira aqui a sua tarefa</h3> <br />
-                <div className='dados-tarefa'>
-                    <input type="checkbox" id="checkbox" /> &nbsp;
-                    <input type="text" id="titulo-tarefa" placeholder="Título" /> &nbsp;
-                    <input type="date" id="data" /> &nbsp;
-                </div>
-                <textarea id="descricao" placeholder='Descrição'></textarea>
+        <form className="tarefa">
+            <div className='dados-tarefa'>
+                <input type="text" id="titulo-tarefa" placeholder="Título" /> &nbsp; &nbsp;
+                <input type="date" id="data" />
             </div>
-            <button type="button">Adicionar</button>
+            <textarea id="descricao" placeholder='Descrição'></textarea>
+            <button type="button" id={'adicionar-tarefa'}>Adicionar</button>
         </form>
     );
 }
