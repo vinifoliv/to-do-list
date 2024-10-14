bd6bfaf6065558e90bab7b630aa387a75ddc7f55
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2024-10-14 19:25:13 -0300

    Rotas para CRUD de tarefas movidas para um controlador.

---
+++ api/controllers/tarefas.js
@@ -0,0 +1,67 @@
+// Imports
+const express = require('express');
+const router = express.Router();
+const { Database } = require('../database');
+const cors = require('cors');
+
+// Configuracoes
+router.use(cors( {origin: '*'} )); // Permite requisicoes de quaisquer origens (sim, a seguranca foi para o beleleu)
+require('dotenv').config({ override: true }); // Carrega as variáveis de ambiente
+const database = new Database();
+
+// Rotas --------------------------------------------------------------------------------------------------------
+// Consulta de tarefas 
+router.get('/consultar-tarefas', async (request, response) => {
+    try {
+        let tarefas = await database.consultarTarefas(1);
+        response.json(tarefas);
+    }
+    catch (error) {
+        console.log(error);
+        response.status(500).send('Erro ao consultar tarefas no banco: ' + error);
+    }
+});
+
+// Adicao de tarefas 
+router.post('/adicionar-tarefa', async (request, response) => {
+    try {
+        let rowCount = await database.adicionarTarefa(request.body); // Objeto com a tarefa
+        
+        console.log(rowCount)
+
+        if (rowCount > 0) response.sendStatus(200);
+        else response.status(400).send('Tarefa inválida.');
+    }
+    catch (error) {
+        console.log(error);
+        response.status(500).send('Erro ao adicionar tarefa no banco: ' + error);
+    }
+});
+
+// Alteracao de tarefas 
+router.put('/alterar-tarefa', async (request, response) => {
+    try {
+        let rowCount = await database.alterarTarefa(request.body); // Objeto com a tarefa
+        if (rowCount > 0) response.sendStatus(200);
+        else response.status(404).send('Tarefa não encontrada.');
+    }
+    catch (error) {
+        console.log(error);
+        response.status(500).send('Erro ao alterar tarefa no banco: ' + error);
+    }
+});
+
+// Remocao de tarefas 
+router.delete('/remover-tarefa/:id', async (request, response) => {
+    try {
+        let rowCount = await database.removerTarefa(request.params.id); // Objeto com o id da tarefa
+        if (rowCount > 0) response.sendStatus(200);
+        else response.status(404).send('Tarefa não encontrada.');
+    }
+    catch (error) {
+        console.log(error);
+        response.status(500).send('Erro ao remover tarefa no servidor: ' + error);
+    }
+});
+
+module.exports = router
--- api/index.js
+++ api/index.js
@@ -1,72 +1,19 @@
-// IMPORTS
+// Imports
 const express = require("express");
 const app = express();
 const cors = require('cors');
-const { Database } = require('./database');
+const tarefasController = require('./controllers/tarefas');
+
 const PORT = 9000;
 
 // Configuracoes --------------------------------------------------------------------------------------
-require('dotenv').config({ override: true }); // Carrega as variáveis de ambiente
-const database = new Database();
 app.use(express.json());
-app.use(cors({ // Garantindo que a requisicao nao seja recusada (sim, a seguranca foi para o beleleu)
-    origin: '*'
-}));
+app.use(tarefasController)
+app.use(cors( {origin: '*'} )); // Permite requisicoes de quaisquer origens (sim, a seguranca foi para o beleleu)
 
 // Rotas --------------------------------------------------------------------------------------------------------
 app.get('/', (request, response) => { // Rota padrao para testes
     response.send('A API está funcionando');
-});
-
-app.get('/consultar-tarefas', async (request, response) => {
-    try {
-        let tarefas = await database.consultarTarefas(1);
-        response.json(tarefas);
-    }
-    catch (error) {
-        console.log(error);
-        response.status(500).send('Erro ao consultar tarefas no banco: ' + error);
-    }
-});
-
-app.post('/adicionar-tarefa', async (request, response) => {
-    try {
-        let rowCount = await database.adicionarTarefa(request.body); // Objeto com a tarefa
-        
-        console.log(rowCount)
-
-        if (rowCount > 0) response.sendStatus(200);
-        else response.status(400).send('Tarefa inválida.');
-    }
-    catch (error) {
-        console.log(error);
-        response.status(500).send('Erro ao adicionar tarefa no banco: ' + error);
-    }
-});
-
-app.put('/alterar-tarefa', async (request, response) => {
-    try {
-        let rowCount = await database.alterarTarefa(request.body); // Objeto com a tarefa
-        console.log(rowCount)
-        if (rowCount > 0) response.sendStatus(200);
-        else response.status(404).send('Tarefa não encontrada.');
-    }
-    catch (error) {
-        console.log(error);
-        response.status(500).send('Erro ao alterar tarefa no banco: ' + error);
-    }
-});
-
-app.delete('/remover-tarefa/:id', async (request, response) => {
-    try {
-        let rowCount = await database.removerTarefa(request.params.id); // Objeto com o id da tarefa
-        if (rowCount > 0) response.sendStatus(200);
-        else response.status(404).send('Tarefa não encontrada.');
-    }
-    catch (error) {
-        console.log(error);
-        response.status(500).send('Erro ao remover tarefa no servidor: ' + error);
-    }
 });
 
 // Rodando o servidor
