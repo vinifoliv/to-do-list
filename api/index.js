ee6b80e288169fc170f5987399a8cd8842ded92e
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2024-10-15 23:48:49 -0300

    Adição de comentários ao index.js e remoção do código para CRUD de tarefas comentado no banco. ^^

--- api/index.js
+++ api/index.js
@@ -1,22 +1,21 @@
-// Imports
+// Imports ------------------------------------------------------------------------------------------------------
 const express = require("express");
 const app = express();
 const cors = require('cors');
 const tarefaController = require('./controllers/tarefaController');
 const usuarioController = require('./controllers/usuarioController');
 
-const PORT = 9000;
-
-// Configuracoes --------------------------------------------------------------------------------------
+// Configurações ------------------------------------------------------------------------------------------------
 app.use(express.json());
 app.use(tarefaController);
 app.use(usuarioController);
 app.use(cors( {origin: '*'} )); // Permite requisicoes de quaisquer origens (sim, a seguranca foi para o beleleu)
+const PORT = 9000;
 
 // Rotas --------------------------------------------------------------------------------------------------------
-app.get('/', (request, response) => { // Rota padrao para testes
+app.get('/', (request, response) => { // Rota para testes
     response.send('A API está funcionando');
 });
 
 // Rodando o servidor
-app.listen(PORT, () => console.log("Servidor rodando em http://localhost:9000"));
+app.listen(PORT, () => console.log("Servidor rodando em http://localhost:" + PORT));
